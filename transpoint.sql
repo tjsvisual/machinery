4bc147c644d7f9154b74fbc03ba8dfbde01a73da
toluwani ademiluyi <toluademiluyi38@gmail.com>
2019-05-18 20:38:50 +0100

    Changes to be committed: 	new file:   PHPMailer/Exception.php 	new file:   PHPMailer/OAuth.php 	new file:   PHPMailer/PHPMailer.php new file:   PHPMailer/POP3.php 	new file:   PHPMailer/SMTP.php 	new file:   forgotPassword.php new file:   functions.php 	new file:   resetPassword.php new file:   transpoint.sql

---
+++ transpoint.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `transaction_point` (
+  `id` int(11) NOT NULL,
+  `t_name` varchar(30) NOT NULL,
+  `t_location` varchar(30) NOT NULL,
+  `t_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
